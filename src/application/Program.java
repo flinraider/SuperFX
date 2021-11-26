b867b8cefa756d6a903ed242c503d6e65808759f
Leonardo Teixeira <leonardo.teixeira.dev@gmail.com>
2021-11-26 13:30:12 -0300

    create menuBar

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import model.exceptions.IOFXException;
@@ -16,9 +17,14 @@ public class Program extends Application {
 	public void start(Stage stage) throws Exception {
 		
 		try {
-			Parent vc = (AnchorPane)FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-			stage.setScene(new Scene(vc));
-			stage.setTitle("View");
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			ScrollPane scrollPane = loader.load(); 
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			stage.setScene(new Scene(scrollPane));
+			stage.setTitle("SuperFX");
 			stage.show();
 		} catch (IOException e) {
 			throw new IOFXException(e.getMessage());
